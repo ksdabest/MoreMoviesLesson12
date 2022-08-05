3d37066441c9a7af23ac46f86c96b1877a5ece21
Yeow Heng <20042741@myrp.edu.sg>
2022-08-05 15:15:49 +0800

    l11 problem statement

--- app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/ModifyMovies.java
+++ app/src/main/java/sg/edu/rp/c346/id21018157/mymovies/ModifyMovies.java
@@ -1,10 +1,13 @@
 package sg.edu.rp.c346.id21018157.mymovies;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -14,6 +17,8 @@ public class ModifyMovies extends AppCompatActivity {
     EditText etID, etTitle, etGenre, etYear;
     Button btnUpdate, btnDelete, btnCancel;
     Spinner ratingSpinner;
+    ArrayAdapter spinnerAdapter;
+    String[] ratingArray;
 
     Movie data;
 
@@ -28,22 +33,28 @@ public class ModifyMovies extends AppCompatActivity {
         etYear = findViewById(R.id.modifyYear);
         ratingSpinner = findViewById(R.id.modifyRatingSpinner);
 
+        ratingArray = new String []{"G", "PG", "PG13", "NC16", "M18", "R21"};
+        spinnerAdapter = new ArrayAdapter(ModifyMovies.this, android.R.layout.simple_spinner_dropdown_item, ratingArray);
+        ratingSpinner.setAdapter(spinnerAdapter);
+
         btnUpdate = findViewById(R.id.btnUpdate);
         btnDelete = findViewById(R.id.btnDelete);
         btnCancel = findViewById(R.id.btnCancel);
-
 
         //initialise variables
         Intent i = getIntent();
         data = (Movie) i.getSerializableExtra("data");
 
+        for(int j = 0; j < ratingArray.length; j++){
+            if(ratingArray[j].equalsIgnoreCase(data.getRating())){
+                ratingSpinner.setSelection(j);
+            }
+        }
+
         etID.setText(String.valueOf(data.getId()));
         etTitle.setText(data.getTitle());
         etGenre.setText(data.getGenre());
         etYear.setText("" + data.getYear());
-
-        String rating = data.getRating();
-
 
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,14 +64,14 @@ public class ModifyMovies extends AppCompatActivity {
                 data.setTitle(etTitle.getText().toString());
                 data.setGenre(etGenre.getText().toString());
                 data.setYear(Integer.parseInt(etYear.getText().toString()));
-
-
-                data.setRating(rating);
+                TextView rating = (TextView) ratingSpinner.getSelectedView();
+                data.setRating(rating.getText().toString());
                 db.updateMovie(data);
                 db.close();
                 finish();
             }
         });
+
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
